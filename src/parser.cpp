7b66392a261578895f35dd5a2ab79e8f4946ce33
MysticAx0lotl <69944483+MysticAx0lotl@users.noreply.github.com>
2026-09-21 18:54:06 -0600

    fix swapped camera/enter effects

--- src/parser.cpp
+++ src/parser.cpp
@@ -370,7 +370,7 @@ static void appendCameraFlip(const Level& level, std::string& outResult)
 
         fmt::format_to(
             std::back_inserter(outResult),
-            "{}23{}{}{};{}1915{}{}{};",
+            "{}24{}{}{};{}1915{}{}{};",
             kGBFData.stringPrefix,
             kGBFData.stringMiddle,
             startXPos,
